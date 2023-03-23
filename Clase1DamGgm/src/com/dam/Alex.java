49e98baaea12d31c644b296507be374ad6df18a8
alexgmz.dev <contacto.alejandrogimenez@gmail.com>
2023-03-23 17:22:57 +0100

    Creacion de clase Alex con errores a solucionar

---
+++ Clase1DamGgm/src/com/dam/Alex.java
@@ -0,0 +1,17 @@
+package src.com.dam;
+/**
+ * 
+ * @author alejandro GS
+ *
+ */
+public class Alex {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("ESTE ES LA CLASE DE ALEJANDRO");
+		/*CONTIENE ERRORES QUE NO SE SOLUCIONAR*/
+
+	}
+
+}
