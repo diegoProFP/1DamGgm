c11f17fa62f54ed3d49a83a6df1e6aeca889f838
Sertra17 <mario.sanchez23@educa.madrid.org>
2023-03-23 21:10:19 +0100

    cambios realizados en Clae1DamGgm

---
+++ Clase1DamGgm/pruebesitas/Prueba.java
@@ -0,0 +1,5 @@
+package pruebesitas;
+
+public class Prueba {
+
+}
---
+++ Clase1DamGgm/src/src/com/dam/Mario.java
@@ -0,0 +1,12 @@
+package src.com.dam;
+
+public class Mario {
+
+	
+	String comewizmi;
+	
+	public void jeilmery() {
+		System.out.println("una vez mas nos vamos a drogar la vida pasa ante mi y la pelicula es muy mala");
+	}
+	
+}
