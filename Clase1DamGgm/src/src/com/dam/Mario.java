668b30ce46c43bb79e6527d837e389e54bb91016
Sertra17 <mario.sanchez23@educa.madrid.org>
2023-03-23 21:12:31 +0100

    illlooo

--- Clase1DamGgm/src/src/com/dam/Mario.java
+++ Clase1DamGgm/src/src/com/dam/Mario.java
@@ -7,6 +7,9 @@ public class Mario {
 	
 	public void jeilmery() {
 		System.out.println("una vez mas nos vamos a drogar la vida pasa ante mi y la pelicula es muy mala");
+	System.out.println("tengo un cuernos de mamut y te lo meto por el culo");
 	}
+	
+	
 	
 }
