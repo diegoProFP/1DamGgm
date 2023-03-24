25ddb4675bbb82750f2fdc688c5069f0cf6511af
juanan30 <alumno@localhost>
2023-03-24 08:51:09 +0100

    prueba1

--- Clase1DamGgm/src/com/dam/Prueba.java
+++ Clase1DamGgm/src/com/dam/Prueba.java
@@ -1,7 +1,11 @@
 package com.dam;
 
 public class Prueba {
-
+/**
+ * prueba1
+ * @author alumno
+ * @param args
+ */
 	public static void main(String[] args) {
 		System.out.println("Hello GGM!");
 
